a7d02a4c8e227ab9f397e326bde6490ca83cacbb
mdyeasinhn <mdyeasinhn@gmail.com>
2025-02-09 17:16:56 +0600

    create booking func added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
-import userRouter from "./module/user/user.route";
+import userRouter from "./module/User/user.route";
 import { StatusCodes } from "http-status-codes";
+import tourRouter from "./module/Tour/tour.route";
 const app: Application = express();
-const port = 3000;
+// const port = 3000;
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+app.use('/api/tour', tourRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
